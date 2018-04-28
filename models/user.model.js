a16dec799f4045d214dd0c0451013c5f865a8f48
LyNnJeR31 <943280824@qq.com>
2018-04-28 23:53:25 +0800

    用户模块

--- models/user.model.js
+++ models/user.model.js
@@ -51,4 +51,112 @@ userSchema.virtual('password').set(function (password) {
   thi.hashedPassword = this.encryptPassword(password);
  }).get(function () { 
    return this._password;
-  });
+});
+
+userSchema.virtual('authInfo').get(function () { 
+  return {
+    'id': this._id,
+    'nickname': this.nickname,
+    'provider': this.provider,
+    'role': this.role,
+    'header': this.header
+  };
+ });
+
+ userSchema.virtual('userInfo').get(function () { 
+   return {
+    'id': this._id,
+    'nickname': this.nickname,
+    'header': this.header,
+    'sex': this.sex,
+    'email': this.email,
+    'showEmail': this.showEmail,
+    'birthdayMonth':this.birthday.month,
+    'birthdayDay':this.birthday.day,
+    'blood': this.blood,
+    'summary':this.summary,
+    'url': this.url,
+    'qqnumber': this.qqnumber,
+    'created': this.created
+   };
+});
+
+userSchema.virtual('providerInfo').get(function () { 
+  return {
+    'github': this.github,
+    'weibo': this.weibo
+  };
+});
+
+userSchema.virtual('token').get(function () { 
+  return {
+    '_id': this._id,
+    'role': this.role
+  };
+});
+
+userSchema.path('nickname').validate(function(value, respond){
+  var self = this;
+  this.constructor.findOne({nickname: value}, function (err, user) { 
+    if (err) {
+      return err;
+    }
+    if (user) {
+      if (self.id === user.id) {
+        return respond(true);
+      }
+      return respond(false);
+    };
+    respond(true);
+    })
+}, '该昵称已被使用');
+
+userSchema.path('email').validate(function(value, respond) {
+  var self = this;
+  this.constructor.findOne({email: value}, function (err, uesr) { 
+    if (err) {
+      return err;
+    }
+    if (user) {
+      if (self.id === user.id) {
+        return respond(true);
+      }
+      return respond(false);
+    };
+    respond(true);
+    })
+},'邮箱已被占用');
+
+userSchema.methods = {
+  // 检查用户权限
+  hasRole: function (role) { 
+    var selfRoles = this.role;
+    return (selfRoles.indexOf('admin') !== -1 || selfRoles.indexOf(role) !== -1);
+  },
+  // 验证密码
+  authenticate: function (plainText) { 
+    return this.encryptPassword(plainText) === this.hashedPassword;
+  },
+  // 生成验证码
+  makeSalt: function () { 
+    return crypto.randomBytes(16).toString('base64');
+  },
+  // 生成密码
+  encryptPassword: function (password) {  
+    if (!password || !this.salt) {
+      return '';
+    }
+    var salt = new Buffer(this.salt, 'base64');
+    return crypto.pbkdf2Sync(password, salt, 10000, 64, 'shal').toString('base64');
+  }
+};
+
+userSchema.set('toObject', {virtuals: true});
+
+var User = mongoose.model('User', userSchema);
+
+var Promise = require('bluebird');
+Promise.promisifyAll(User);
+Promise.promisifyAll(User.prototype);
+
+module.exports = User;
---
+++ routes/user/index.js
@@ -0,0 +1,16 @@
+var express = reuqire('express');
+var controller = require('./user.controller');
+var expressJwt = require('express-jwt');
+var config = require('../../config');
+
+var router = express.router();
+
+router.post('/addUser', controller.addUser);
+router.get('/authInfo', expressJwt({secret: config.session.secrets}, controller.authInfo));
+router.get('/set', expressJwt({secret: config.session.secrets}),  controller.userSet);
+router.get('/:id/userInfo', expressJwt({secret: config.session.secrets,credentialsRequired:false}), controller.userInfo);
+router.post('/header', expressJwt({secret: config.session.secrets}), controller.header);
+router.put('/updateUser', expressJwt({secret: config.session.secrets}), controller.updateUser);
+router.put('/updatePassword', expressJwt({secret: config.session.secrets}), controller.updatePassword);
+
+module.exports = router;
